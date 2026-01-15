89029dd89e485d0718e1e4d84a2ba06d8cc187b6
wave <flashingcards69@gmail.com>
2026-01-15 04:59:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6072428518-2e28a",
+  "appId": "1:34456202028:web:88053b973626717e9ada89",
+  "apiKey": "AIzaSyAmbpO_3GIZ46AyvmAZNZMOXOB4qWA2kKg",
+  "authDomain": "studio-6072428518-2e28a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "34456202028"
+};
